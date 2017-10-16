0d0d07aef6fd5f1055fe14caeff5cf6135708ba1
dandolendres <cynthiatalde@gmail.com>
2017-10-16 22:31:43 +0800

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,25 @@
 @section('content')
     <link rel="stylesheet" href="{{ asset('css/pricing/style.css') }}">
     <section>
-        ABOUT US
+	<p>Steve Jobs (Founder of Apple Inc) once said 
+		"Everybody in this country must learn how 
+		to program a computer because it teaches you
+		how to think ".So the Code Latte created, 
+		it is an E-learning system that will help
+		encourage everyone that want to make things
+		probably easy. I’m El Miguel commonly 
+		known as Migz - individual with passion for
+		learning and coding — coding makes me happy.
+		I truly believe that learning how to code 
+		has a big influence in every bodies daily life
+		but it doesn’t mean that everyone must work as 
+		a developer or programmer. Coding thought everyone 
+		to solve different problem like how you think what's
+		the error on your program? Otherwise how to boost your
+		critical thinking under pressure like how you think the correct 
+		syntax. In short you creating yourself as a solution-driven individual. </p>
+	<p>El Miguel A. Marcaida (Founder of Code Latte)</p>
+      
     </section>
 
 
