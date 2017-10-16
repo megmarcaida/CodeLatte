36fecc87e9f9bd1f5beaceff1076b3c3bec1eb4a
dandolendres <cynthiatalde@gmail.com>
2017-10-17 00:03:17 +0800

    test

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -1,9 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-    <link rel="stylesheet" href="{{ asset('css/pricing/style.css') }}">
+   
     <section>
-        gallery
+ <p> Alaina Wilson, Training Coordinator 
+Hohman Plating.
+This feature is amazing! I can definitely see this as a life changer especially when we can do and understand the lessons and complete these observations on their own.
+Ariane Butcher, Corporate Trainer 
+Winterwood, Inc.
+We are having a lot of positive feedback about your new LMS.  It is so exciting to have such great results from our employees. Recently we received a suggestion from a supervisor for Winterwood.  She requested access to see her employee activity results for the team of managers she supervises and e-learning had this feature right there.
+Alexys Cran,
+Student Services Coordinator,
+Cornerstone International College
+Adding and removing subscription capabilities to users has been helpful for our online training programs. Mainly it has given me more access without having hesitate wit users who were no longer allowed Supervisor access to the online program allowing only the right users access to training and compliance documentation reports. The savings in terms of hours spent getting them these reports as well as the overall ease of use means we can dedicate our resources to serving our users and focusing on our core mission.
+Trina McKie,
+President,
+NPLI
+The Code Latter feature is very useful! My favorite feature is that codeblock are able to see the completion reports, rather than the admin having to send them.
+Richard Underwood,
+Quality Assurance Professional
+Quanticate
+We recently rolled out the e-learning solution with 100+ courses and 200+ trainees across our 9 offices in 6 different countries. Roll out was flawless and the trainees have stated how user-friendly the system is to use. Support in setting up the system, and ongoing support in maintaining the live system has been second to none, with support staff being available out of standard office hours has been ideal as a UK-headquartered company.
+Nate Barin,
+Senior Publication Specialist,
+Halozyme Therapeutics
+The Code latte  training system is very user-friendly, and it significantly improved our current training process. The thing that was most impressive to me was the customer service. It’s very rare that a CEO would take the time to sit with a client and help them with their every need. Don Weobong was instrumental during the implementation of eLearning for our company. He was very quick to answer any questions we had and helped us with any issues we ran into during implementation. I would recommend eLearning to any company that is looking to move to a web-based training system.
+Maurice B. Faust, 
+Advanced Training and Education Systems
+Hello!
+I am writing this letter to express my utmost appreciation for the great work that the eLearning Team has done my business.
+About a year ago I became unemployed at age 52 and had no idea what I was going to do. I decided to look for a company that could help me present my skills online. I had spent numerous years as a Training Manager but, my experience was primarily hands-on i.e. non-technical. Finding eLearning training made the development and implementation of my business not only possible but actually fun. They made it easier and understandable for a non-technical guy like me.
+All I can say is thanks Code Latte!
+Amy Hitchman, Training Manager,
+Host Analytics
+The Code latter training platform has streamlined our processes, reduced costs, and provided our users with a comprehensible learning system. I was able to configure and launch this SaaS based LMS within weeks, using SCORM to effectively track user progress with built in reports. Thanks to the entire eLearning staff, your support and response time is exceptional.
+Monte Montoya,
+cPrime Inc.
+Wow! Thank you so much.
+Your service Code Latte has been amazing.
+Sonya Culver,
+Curriculum & Training Specialist
+Code latte is a very user-friendly program for both administrators and trainees. The customer service is superb as timely responses are provided often with a tailor-made voice explanation of your specific concern to ensure your implementation runs effectively. I strongly recommend the services provided by elearning communities for any size company for training and/or media communication medium.
+Darren Dolcemascolo,
+EMS Consulting Group
+The eLearning program is excellent. 
+eLearning made it easy for us to create self-paced training programs from our already 
+</p>
+
     </section>
 
 
